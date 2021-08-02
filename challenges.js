ad41434038839f9b40e2ef5deebb144ba92a2a9d
lfoster1150 <lfoster1150@gmail.com>
2021-08-02 09:31:04 -0500

    Passed fourth problem

--- challenges.js
+++ challenges.js
@@ -76,7 +76,13 @@ sumNumbers([2, 10, -5]) //=> 7
 sumNumbers([]) //=> 0
 -----------------------------------------------------------------*/
 // Your solution for 03-sumNumbers here:
-function sumNumbers(nums) {}
+function sumNumbers(nums) {
+  let sum = 0
+  for (let i = 0; i < nums.length; i++) {
+    sum += nums[i]
+  }
+  return sum
+}
 /*-----------------------------------------------------------------
 Challenge: 04-addList
 
